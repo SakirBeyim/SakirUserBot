755c6e20e589f6836e64450c9b975c2329497baf
SakirBeyim <86803005+SakirBeyim@users.noreply.github.com>
2021-07-01 23:18:49 +0300

    Add files via upload

---
+++ windows_startup_script.py
@@ -0,0 +1,8 @@
+dan  teleton  ithalat  TelegramClient
+API_KEY = "Bunu buraya doldurun "
+API_HASH = "Bunu buraya doldurun "
+# my.telegram.org adresinden alın
+bot  =  TelegramClient ( 'userbot' , API_KEY , API_HASH )
+bot . başlangıç ()
+
+#Bu komut dosyası botunuzu çalıştırmaz, sadece bir oturum oluşturur.
